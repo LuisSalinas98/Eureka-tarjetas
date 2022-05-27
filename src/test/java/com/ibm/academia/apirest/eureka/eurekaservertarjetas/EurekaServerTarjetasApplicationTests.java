cf5e470452471e72f52a0e42d93441132cccf069
LuisSalinas <LASR-98@ibm.com>
2022-05-26 20:23:55 -0500

    Se subió el server de eureka a git

---
+++ src/test/java/com/ibm/academia/apirest/eureka/eurekaservertarjetas/EurekaServerTarjetasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.academia.apirest.eureka.eurekaservertarjetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerTarjetasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
